39cb9ae5629d6373b9d950ff42c53fdd69e95335
Ryan Catalani <catalani.ryan@gmail.com>
2011-11-16 15:25:28 -0500

    Added twitter field to people

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111116202134) do
+ActiveRecord::Schema.define(:version => 20111116202459) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(:version => 20111116202134) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "staff"
+    t.string   "twitter"
   end
 
 end
