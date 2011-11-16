26858f1bc1fd1564e72bbdce105eef70a611e895
Ryan Catalani <catalani.ryan@gmail.com>
2011-11-16 15:22:18 -0500

    Added staff boolean to people

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111116192909) do
+ActiveRecord::Schema.define(:version => 20111116202134) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(:version => 20111116192909) do
     t.boolean  "editor"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "staff"
   end
 
 end
