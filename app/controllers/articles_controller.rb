287af6c0055da26670e1c96f0441c5406fb7f158
Ryan Catalani <catalani.ryan@gmail.com>
2011-12-14 18:56:01 -0500

    said authors when I meant people

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -66,7 +66,7 @@ class ArticlesController < ApplicationController
 	def edit
 		# TODO: Doesn't update authors, etc
 		@article = Article.find(params[:id])
-		@current_authors = @article.authors
+		@current_authors = @article.people
 		@sections = Section.all.map { |s| [s.name, s.id] }
 		@authors = Person.order("lastname ASC").all.map { |person| [person.official_name, person.id] }
 	end
