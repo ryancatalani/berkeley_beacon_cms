63c023349aea93fff213b096cc1cd83f60d5b01d
Ryan Catalani <catalani.ryan@gmail.com>
2012-02-02 01:15:31 -0500

    Changed config.assets.compile back to false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ BeaconApp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
+# ------- CHANGE THIS BACK TO FALSE SOMETIME! -------
 
   # Generate digests for assets URLs
   config.assets.digest = true
