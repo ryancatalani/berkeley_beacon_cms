0486476b6bb6d6409f5b6a71c35d87bcb58da2b7
Ryan Catalani <catalani.ryan@gmail.com>
2012-03-08 09:16:42 -0800

    Changed api endpoints to apigee

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,20 @@
+if ENV['APIGEE_TWITTER_API_ENDPOINT']
+	endpoint = ENV['APIGEE_TWITTER_API_ENDPOINT']
+else
+	endpoint = "***REMOVED***"
+end
+
+if ENV['APIGEE_TWITTER_SEARCH_API_ENDPOINT']
+	search_endpoint = ENV['APIGEE_TWITTER_SEARCH_API_ENDPOINT']
+else
+	search_endpoint = "***REMOVED***"
+end
+
 Twitter.configure do |config|
 	config.consumer_key = "***REMOVED***"
   config.consumer_secret = "***REMOVED***"
   config.oauth_token = "***REMOVED***"
   config.oauth_token_secret = "***REMOVED***"
+  config.endpoint = "http://" + endpoint
+  config.search_endpoint = "http://" + search_endpoint
 end
