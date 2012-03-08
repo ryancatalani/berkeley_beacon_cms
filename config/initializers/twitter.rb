015c738e0b518207844984b4914efe84668e2f6e
Ryan Catalani <catalani.ryan@gmail.com>
2012-03-08 13:08:14 -0800

    Took out apigee endpoints for now

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -15,6 +15,6 @@ Twitter.configure do |config|
   config.consumer_secret = "***REMOVED***"
   config.oauth_token = "***REMOVED***"
   config.oauth_token_secret = "***REMOVED***"
-  config.endpoint = "http://" + endpoint
-  config.search_endpoint = "http://" + search_endpoint
+  # config.endpoint = "http://" + endpoint
+  # config.search_endpoint = "http://" + search_endpoint
 end
