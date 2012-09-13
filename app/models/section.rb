1184807523d4c82ca2ff57e3a30573a84f203388
Ryan Catalani <catalani.ryan@gmail.com>
2012-09-12 21:53:05 -0400

    Changed draft filtering to work with PG.

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,7 +2,7 @@ class Section < ActiveRecord::Base
 	has_many :articles
 
 	def non_blog_articles
-		self.articles.where(:blog_id => [0, nil], :draft => [0, nil])
+		self.articles.where(:blog_id => [false, nil], :draft => [false, nil])
 	end
 
 end
