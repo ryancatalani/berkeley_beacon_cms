524efddaa7c5a135020f9185d3fb759af01ab988
Ryan Catalani <catalani.ryan@gmail.com>
2012-10-25 15:38:56 -0400

    Still working on straightening poll results

--- app/models/political_poll_entry.rb
+++ app/models/political_poll_entry.rb
@@ -45,7 +45,16 @@ class PoliticalPollEntry < ActiveRecord::Base
 		opts = create_options(which)
 		opts.each {|o| results << [o, 0]}
 		all.each do |r|
-			choice = r.send("q#{qnum}")
+			fnum = qnum
+			logger.debug r.id
+			logger.debug qnum
+			logger.debug results
+			# if Rails.env.production? and r.id < 62 and qnum > 11
+			if qnum > 12
+				fnum -= 1
+			end
+			choice = r.send("q#{fnum}")
+			logger.debug choice
 			choice = -1 if choice.nil?
 			results[choice+1][1] += 1
 		end
