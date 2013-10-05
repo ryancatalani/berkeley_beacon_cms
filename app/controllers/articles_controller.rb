0294d99d9fe6725282f2ed72df957c85888229fe
Ryan Catalani <catalani.ryan@gmail.com>
2013-10-04 21:58:05 -0400

    Automatically select the latest issue.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
 		@sections = Section.all.map { |s| [s.name, s.id] }
 		@series = [["None",0]] + Series.all.map {|s| [s.title, s.id] }
 		@blogs = [["None", 0]] + Blog.all.map {|b| [b.title, b.id] }
-		@issues = [["None", 0]] + Issue.all.map {|i| [i.release_date_f, i.id]}.reverse
+		@issues = Issue.all.map {|i| [i.release_date_f, i.id]}.reverse.insert(1,["None/Online only", 0])
 		@can_queue_tweet = can_queue_tweet?
 	end
 
@@ -82,7 +82,7 @@ class ArticlesController < ApplicationController
 			@sections = Section.all.map { |s| [s.name, s.id] }
 			@series = [["None",0]] + Series.all.map {|s| [s.title, s.id] }
 			@blogs = [["None", 0]] + Blog.all.map {|b| [b.title, b.id] }
-			@issues = [["None", 0]] + Issue.all.map {|i| [i.release_date_f, i.id]}.reverse
+			@issues = Issue.all.map {|i| [i.release_date_f, i.id]}.reverse.insert(1,["None/Online only", 0])
 			render "new"
 		end
 	end
@@ -169,7 +169,7 @@ class ArticlesController < ApplicationController
 		@authors = Person.order("firstname ASC").all.map { |person| [person.official_name, person.id] }
 		@authors.unshift(["Choose an author",0])
 		@series = [["None",0]] + Series.all.map {|s| [s.title, s.id] }
-		@issues = [["None", 0]] + Issue.all.map {|i| [i.release_date_f, i.id]}.reverse
+		@issues = Issue.all.map {|i| [i.release_date_f, i.id]}.reverse.insert(1,["None/Online only", 0])
 		@can_queue_tweet = can_queue_tweet?
 	end
 
@@ -229,7 +229,7 @@ class ArticlesController < ApplicationController
 			@authors = Person.order("lastname ASC").all.map { |person| [person.official_name, person.id] }
 			@display_already_uploaded = true unless cookies[:already_uploaded].nil? or cookies[:already_uploaded].blank?
 			@series = [["None",0]] + Series.all.map {|s| [s.title, s.id] }
-			@issues = [["None", 0]] + Issue.all.map {|i| [i.release_date_f, i.id]}.reverse
+			@issues = Issue.all.map {|i| [i.release_date_f, i.id]}.reverse.insert(1,["None/Online only", 0])
 			render 'edit'
 		end
 	end
