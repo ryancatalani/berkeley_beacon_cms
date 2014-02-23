5406b785b094773673714ee5dbe2b520c66acbe2
Ryan Catalani <ryan@ryancatalani.com>
2014-02-23 12:58:18 -0500

    Prototype: Hide other section articles if there aren't any, display feature correctly

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -316,7 +316,7 @@ class ArticlesController < ApplicationController
 		if params[:a14]=="true" && editor_logged_in
 			@body_class = "a14_article"
 			@section_issue_articles = Article.where(:issue_id => Issue.latest.id, :section_id => @article.section.id).all.delete_if {|a| a.id == @article.id }
-			render 'show2014', :layout => 'article2014'
+			render('show2014', :layout => 'article2014') && return
 		end
 
 		if @article.section and @article.section.name == "Feature"
