1e82c694547992c6906f26bc326d86d4279185b1
Ryan Catalani <ryan@ryancatalani.com>
2014-02-23 00:08:12 -0500

    One more fix for Swiftype

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -146,13 +146,14 @@ class PagesController < ApplicationController
 
 	def search
 		@title = "Search results: #{params[:q]}"
-		engine_slug = Rails.env.production? ? "berkeleybeacon" : "berkeleybeaconsandbox"
+		# engine_slug = Rails.env.production? ? "berkeleybeacon" : "berkeleybeaconsandbox"
+		engine_slug = "berkeleybeacon"
 		# engine = Swiftype::Engine.find(engine_slug)
 		if params[:q]
 			client = Swiftype::Easy.new
 			results = client.search(engine_slug, params[:q])
-			@articles = results.records['articles'].map{|r| Article.find(r.external_id.to_i) rescue nil } #.paginate(:page => params[:page], :per_page => 15)
-			@mediafiles = results.records['mediafiles'].map{|r| Mediafile.find(r.external_id.to_i) rescue nil }
+			@articles = results.records['articles'].map{|r| Article.find(r.external_id.to_i) rescue nil }.delete_if{ |a| a.nil? } #.paginate(:page => params[:page], :per_page => 15)
+			@mediafiles = results.records['mediafiles'].map{|r| Mediafile.find(r.external_id.to_i) rescue nil }.delete_if{ |m| m.nil? }
 		else
 			@articles = []
 			@mediafiles = []
