9aff7b02a7dc8a5520efa556d213891821b81721
Ryan Catalani <catalani.ryan@gmail.com>
2014-09-20 19:21:59 -0400

    Changed namespace declaration in latest issue RSS feed. Only use top (i.e. homepage) articles.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -51,17 +51,22 @@ class IssuesController < ApplicationController
 
 	def latest_issue_rss
 		articles_unsorted = Issue.latest.articles
+		home = HomeLayout.last.articles
 		@issue = Issue.latest
-		@home = HomeLayout.last
+		
 		@articles = []
 
-		@articles << articles_unsorted.where(section_id: Section.find_by_name('News').id)
-		@articles << articles_unsorted.where(section_id: Section.find_by_name('Feature').id)
-		@articles << articles_unsorted.where(section_id: Section.find_by_name('Opinion').id)
-		@articles << articles_unsorted.where(section_id: Section.find_by_name('Arts').id)
-		@articles << articles_unsorted.where(section_id: Section.find_by_name('Lifestyle').id)
-		@articles << articles_unsorted.where(section_id: Section.find_by_name('Sports').id)
+		# @articles << articles_unsorted.where(section_id: Section.find_by_name('News').id)
+		# @articles << articles_unsorted.where(section_id: Section.find_by_name('Feature').id)
+		# @articles << articles_unsorted.where(section_id: Section.find_by_name('Opinion').id)
+		# @articles << articles_unsorted.where(section_id: Section.find_by_name('Arts').id)
+		# @articles << articles_unsorted.where(section_id: Section.find_by_name('Lifestyle').id)
+		# @articles << articles_unsorted.where(section_id: Section.find_by_name('Sports').id)
 		
+		@articles << Article.find(home[:lead])
+		@articles << home[:featured].map{|id| Article.find(id)}
+		@articles << home[:middle].map{|id| Article.find(id)}
+
 		@articles.flatten!
 		render :layout => false
 	end
