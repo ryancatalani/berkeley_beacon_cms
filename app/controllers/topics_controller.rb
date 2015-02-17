a2fc7dbdabb462a8127e7bb2f8e3881dfd4b244e
Ryan Catalani <catalani.ryan@gmail.com>
2015-02-17 14:32:27 -0500

    Added action to show topic articles.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,10 +1,21 @@
 class TopicsController < ApplicationController
   def show
+    @include_responsive = true
+    begin
+      topic = Topic.find_by_slug params[:slug]
+      @sname = topic.title
+      @articles = topic.articles.paginate(:page => params[:page], :per_page => 15).order("created_at DESC")
+      @show_sname = true
+      render "sections/show"
+    rescue
+      redirect_to root_path
+    end
   end
 
   def create
   	p = params[:topic]
   	@topic = Topic.new(p)
+    @topic.slug = @topic.title.downcase.gsub(' ','-').gsub(/-\W/,'')
   	if @topic.save
   		respond_to do |f|
   			f.html { redirect_to articles_path, :notice => "Saved topic" }
