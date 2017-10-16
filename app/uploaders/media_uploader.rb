56b3a251f0abc3f6d1cccc8dd1bdcf5480610890
Ryan Catalani <catalani.ryan@gmail.com>
2017-10-16 14:21:32 -0400

    Update carrierwave settings.

--- app/uploaders/media_uploader.rb
+++ app/uploaders/media_uploader.rb
@@ -59,6 +59,10 @@ class MediaUploader < CarrierWave::Uploader::Base
   #   "#{Rails.public_path}/system/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   # end
 
+  def store_dir
+    "beacon_uploads/uploads"
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,11 @@
 CarrierWave.configure do |config|
   config.cache_dir = "#{Rails.root}/tmp/uploads"
   config.storage    = :aws
+  
   # config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
   config.aws_bucket = 'BerkeleyBeacon/beacon_uploads'
+  config.aws_bucket = 'BerkeleyBeacon'
+
   config.aws_acl    = 'public_read'
   # config.asset_host = 'http://example.com'
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
