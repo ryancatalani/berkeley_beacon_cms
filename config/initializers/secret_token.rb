410fb7fbee34f0f0b6a54b2ad29d6994a33d436c
Ryan Catalani <catalani.ryan@gmail.com>
2017-10-16 12:17:19 -0400

    Run rake rails:update.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BeaconApp::Application.config.secret_token = 'e7fdf076a5a4e0b5a77e75eac0c2ad3db74acfd91334f9d00cad862d4218c6dbee75e8edc2600def4d6cd067edaad79420e3319c0a98223ef2b4f9ef71970137'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeaconApp::Application.config.secret_key_base = '***REMOVED***'
